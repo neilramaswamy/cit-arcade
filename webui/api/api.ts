5830ee9e0fe973d7643f5453a6e4de9132bbec27
Neil Ramaswamy <neil@ramaswamy.org>
2023-03-27 18:09:52 -0400

    log fe url

--- webui/api/api.ts
+++ webui/api/api.ts
@@ -2,6 +2,7 @@ import { join } from "path";
 
 const getAPIClient = () => {
   const apiUrl = process.env.NEXT_PUBLIC_API_URL || "";
+  console.log(`API URL is ${apiUrl}`);
 
   const sendRequest = async (path: string) => {
     const response = await fetch(join(apiUrl, path));
