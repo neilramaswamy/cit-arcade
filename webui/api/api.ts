0fb0852b9d704de9a5e6cf5cb83026956c28abfe
Neil Ramaswamy <neil@ramaswamy.org>
2023-03-27 18:23:10 -0400

    force rebuild

--- webui/api/api.ts
+++ webui/api/api.ts
@@ -2,7 +2,7 @@ import join from "url-join";
 
 const getAPIClient = () => {
   const apiUrl = process.env.NEXT_PUBLIC_API_URL || "";
-  console.log(`API URL is ${apiUrl}`);
+  console.log(`API URL is: ${apiUrl}`);
 
   const sendRequest = async (path: string) => {
     const destination = join(apiUrl, path);
