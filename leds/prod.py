dbf76ecc5f0efdbe1bc376f5a56d0ed786bdcd19
Neil Ramaswamy <neil@ramaswamy.org>
2023-03-27 12:59:05 -0400

    update led count

--- leds/prod.py
+++ leds/prod.py
@@ -1,5 +1,5 @@
 # LED strip configuration:
-LED_COUNT = 16        # Number of LED pixels.
+LED_COUNT = 90        # Number of LED pixels.
 LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
