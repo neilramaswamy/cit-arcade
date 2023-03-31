4f74a33c90b397eb4307f3fa8c05534519191d7e
Nathan Benavides-Luu <nate1299@me.com>
2023-03-31 15:22:47 -0400

    first draft of controller

--- webui/pages/index.tsx
+++ webui/pages/index.tsx
@@ -1,44 +1,111 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-import { ApiClient } from "@/api/api";
+import {ApiClient} from "@/api/api";
+import {useCallback, useEffect} from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+const BUTTON = {
+    UP: 1,
+    DOWN: 2,
+    LEFT: 3,
+    RIGHT: 4,
+    A: 5,
+    B: 6,
+    MIDDLE1: 7,
+    MIDDLE2: 8,
+}
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>The CIT Arcade</title>
-        <meta name="description" content="Made with <3 by NSZ" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    function onButtonClick(buttonNumber: number) {
+        console.log("Button clicked: " + buttonNumber);
+        // ApiClient.sendRequest("/up");
+    }
 
-      <main className={styles.main}>
-        <h2>The CIT Arcade</h2>
+    const handleKeyPress = useCallback((event) => {
+        console.log(`Key pressed: ${event.key}`);
+        switch (event.key) {
+            case 'w':
+                onButtonClick(BUTTON.UP);
+                break;
+            case 'a':
+                onButtonClick(BUTTON.LEFT);
+                break;
+            case 's':
+                onButtonClick(BUTTON.DOWN);
+                break;
+            case 'd':
+                onButtonClick(BUTTON.RIGHT);
+                break;
+            case 'j':
+                onButtonClick(BUTTON.A);
+                break;
+            case 'k':
+                onButtonClick(BUTTON.B);
+                break;
+        }
+    }, []);
 
-        <div>
-          <button
-            onClick={() => {
-              ApiClient.sendRequest("/up");
-            }}
-          >
-            Go up
-          </button>
-        </div>
+    useEffect(() => {
+        // attach the event listener
+        document.addEventListener('keydown', handleKeyPress);
 
-        <div>
-          <button
-            onClick={() => {
-              ApiClient.sendRequest("/down");
-            }}
-          >
-            Go down
-          </button>
-        </div>
-      </main>
-    </>
-  );
+        // remove the event listener
+        return () => {
+            document.removeEventListener('keydown', handleKeyPress);
+        };
+    }, [handleKeyPress]);
+
+    return (
+        <>
+            <Head>
+                <title>The CIT Arcade</title>
+                <meta name="description" content="Made with <3 by NSZ"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+
+            <main>
+                <h2>The CIT Arcade</h2>
+
+                <div className="flex justify-between items-end max-w-2xl p-8 bg-gray-100 mx-auto">
+                    {/*D Pad*/}
+                    <div>
+                        <div className="flex justify-center">
+                            <button className="h-16 w-16 bg-neutral-400 rounded-t-lg"
+                                    onClick={() => onButtonClick(BUTTON.UP)}/>
+                        </div>
+                        <div className="flex justify-center">
+                            <button className="h-16 w-16 bg-neutral-400 rounded-l-lg"
+                                    onClick={() => onButtonClick(BUTTON.LEFT)}/>
+                            <div className="h-16 w-16 bg-neutral-400"/>
+                            <button className="h-16 w-16 bg-neutral-400 rounded-r-lg"
+                                    onClick={() => onButtonClick(BUTTON.RIGHT)}/>
+                        </div>
+                        <div className="flex justify-center">
+                            <button className="h-16 w-16 bg-neutral-400 rounded-b-lg"
+                                    onClick={() => onButtonClick(BUTTON.DOWN)}/>
+                        </div>
+                    </div>
+
+                    {/*Middle Buttons*/}
+                    <div>
+                        <div className="flex justify-center space-x-4">
+                            <button className="h-6 w-16 bg-neutral-400 rounded-full"
+                                    onClick={() => onButtonClick(BUTTON.MIDDLE1)}/>
+                            <button className="h-6 w-16 bg-neutral-400 rounded-full"
+                                    onClick={() => onButtonClick(BUTTON.MIDDLE2)}/>
+                        </div>
+                    </div>
+
+                    {/*Side Buttons*/}
+                    <div>
+                        <div className="flex justify-center space-x-4">
+                            <button className="h-16 w-16 bg-red-600 rounded-full"
+                                    onClick={() => onButtonClick(BUTTON.A)}/>
+                            <button className="h-16 w-16 bg-red-600 rounded-full"
+                                    onClick={() => onButtonClick(BUTTON.B)}/>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    );
 }
