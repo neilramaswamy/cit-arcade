123e5c3cfb21cfa44e288cf5f17d1661e79dfda7
Neil Ramaswamy <neil.ramaswamy@databricks.com>
2024-03-17 10:51:59 -0400

    Only the best gifs

--- game/games/gifs.py
+++ game/games/gifs.py
@@ -22,7 +22,7 @@ class GifGame(AbstractMiniGame):
         # The number of full iterations we've done so far for the current gif
         self.loops_for_curr_gif = 0
 
-        self.gifs = ["congrats", "blueno", "nyancat", "fire", "stars"]
+        self.gifs = ["rainbow", "blueno", "sonic", "nyancat", "fire"]
 
         self.height = self.screen.get_height()
         self.width  = self.screen.get_width()
